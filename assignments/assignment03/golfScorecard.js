9e26436f6a6a40104f2b55d751ac5872bc92ded8
BuckOfTheEye <89933612+BuckOfTheEye@users.noreply.github.com>
2021-10-19 00:05:09 -0400

    Update golfScorecard.js

--- assignments/assignment03/golfScorecard.js
+++ assignments/assignment03/golfScorecard.js
@@ -13,8 +13,9 @@ let elem = [];
 for(let i=1; i<=18; i++) {
   // console.log(i);
   elem[i] = document.getElementById(i.toString());
+  elem[1].children[4].children[0].onclick = function(){add1(elem[i]);};
 }
-elem.children[4].children[0].onclick = function(){add1(elem[i]);};
+
 
 // create an "add1" function
 function add1 (elem) {
