8a6c2c3202eb37d9856e0a02e2904aafd90a6945
BuckOfTheEye <89933612+BuckOfTheEye@users.noreply.github.com>
2021-10-19 08:33:58 -0400

    Update golfScorecard.js

--- assignments/assignment03/golfScorecard.js
+++ assignments/assignment03/golfScorecard.js
@@ -31,7 +31,7 @@ for(let i=1; i<=18; i++) {
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") {
     elem.children[2].innerHTML = "1";
-  else {
+    else {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore + 1;
